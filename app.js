5059ccc9db18b6c3faafe55ea2e2085ff3b23415
akanksha <akankshakh00@gmail.com>
2023-12-05 15:48:59 +0530

    emitting events and creating server, listening on server

--- app.js
+++ app.js
@@ -1,22 +1,79 @@
 // BUILT IN MODULES
-const os = require('os');
+// const os = require('os');
 
-const obj = {
-    name: os.type(),
-    release: os.release(),
-    totalMemory: os.totalmem(),
-    freeMemory : os.freemem(),
-}
-console.log(obj)
+// const obj = {
+//     name: os.type(),
+//     release: os.release(),
+//     totalMemory: os.totalmem(),
+//     freeMemory : os.freemem(),
+// }
+// console.log(obj)
 
-const path = require('path');
-console.log(path);
+// const path = require('path');
+// console.log(path);
 
-const filePath = path.join('content', '/subholder', 'test.txt');
-console.log(filePath);
+// const filePath = path.join('content', '/subholder', 'test.txt');
+// console.log(filePath);
 
-const absolutePath = path.resolve('content', 'subfolder', 'test.txt');
-console.log(absolutePath);
+// const absolutePath = path.resolve('content', 'subfolder', 'test.txt');
+// console.log(absolutePath);
 
 // const file = require('fs');
 // console.log(file);
+
+const { createServer } = require('http');
+const server = createServer((req, res) => {
+    if (req.url === '/') {
+        res.end('Home Page');
+    }
+    else (req.url === 'about')
+    {
+        res.end('About Page');
+    }
+    res.end('Error Page');
+    // res.write('Welcome to our home page');
+    res.end();
+});
+
+server.listen(5000, ()=>{
+    console.log('Server is listening on port 5000');
+});
+
+const {readFile} = require('fs');
+
+readFile('./content/first.txt', 'utf-8', (err, data)=>{
+    if (err){
+        return
+    }
+    else {
+        console.log(data);
+    }
+})
+const getText = (path)=>{
+ return new Promise((resolve, reject)=>{
+    readFile(path, 'utf-8', (err, data)=>{
+        if(err){
+         reject(err);
+        }
+        else{
+            resolve(data);
+        }
+    })
+ })
+}
+getText('./content/first.txt');
+
+const start = async () =>{
+    const first = await getText ('./content/first.txt')
+    console.log(first);
+}
+start();
+
+const EventEmitter = require('events');
+
+const customEmitter = new EventEmitter();
+
+customEmitter.on('response', ()=>{
+    console.log(`data recieved`);
+});
+customEmitter.emit('response');
